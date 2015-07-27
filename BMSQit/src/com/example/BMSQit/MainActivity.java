4d2ecb50321e2612429cf4907e8c5bbfc4514d25
Rohith Kumar <rohithkumar31@gmail.com>
2015-07-27 15:48:37 +0530

    first commit

---
+++ BMSQit/src/com/example/BMSQit/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.BMSQit;
+
+import com.example.BMSQit.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Thread t = new Thread(){
+			public void run(){
+				try{
+					sleep(2000);
+				}catch(Exception e){
+					e.printStackTrace();
+				}finally{
+					Intent i = new Intent(MainActivity.this,Login.class);
+					startActivity(i);
+				}
+			}
+		};
+		
+		t.start();
+	}
+
+	
+}
